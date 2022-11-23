92884f501e25f5e24b16d21c95111303d43fd326
Mikhail <mikhail.eremeev.work@gmail.com>
2022-11-23 18:37:59 +0300

    Made code for Sprite.cpp and added new vertex and fragment shaders for sprite

--- src/Renderer/Sprite.h
+++ src/Renderer/Sprite.h
@@ -15,8 +15,8 @@ namespace Renderer
 		Sprite(
 			const std::shared_ptr<Texture2D> pTexture,
 			const std::shared_ptr<ShaderProgram> pShaderProgram,
-			const glm::vec2& position = glm::vec2(0.f),
-			const glm::vec2& size = glm::vec2(1.f),
+			const glm::vec2 &position = glm::vec2(0.f),
+			const glm::vec2 &size = glm::vec2(1.f),
 			const float rotation = 0.f
 		);
 
@@ -33,8 +33,11 @@ namespace Renderer
 	private:
 		std::shared_ptr<Texture2D> m_pTexture;
 		std::shared_ptr<ShaderProgram> m_pShaderProgram;
-		glm::vec2& m_position;
-		glm::vec2& m_size;
+		glm::vec2 m_position;
+		glm::vec2 m_size;
 		float m_rotation;
+		GLuint m_VAO;
+		GLuint m_vertexCoordsVBO;
+		GLuint m_textureCoordsVBO;
 	};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "Renderer/ShaderProgram.h"
 #include "Resources/ResourceManager.h"
 #include "Renderer/Texture2D.h"
+#include "Renderer/Sprite.h"
 
 GLfloat point[] = {
       0.0f,  50.0f, 0.0f,
@@ -95,7 +96,17 @@ int main(int argc, char** argv)
             return -1;
         }
 
+        auto pSpriteShaderProgram = resourceManager.loadShaders("SpriteShader", "res/shaders/vSprite.txt", "res/shaders/fSprite.txt");
+        if (!pSpriteShaderProgram)
+        {
+            std::cerr << "Can't create shader program: " << "SpriteShader" << std::endl;
+            return -1;
+        }
+
         auto tex = resourceManager.loadTexture("DefaultTexture", "res/textures/map_16x16.png");
+
+        auto pSprite = resourceManager.loadSprite("NewSprite", "DefaultTexture", "SpriteShader", 50, 100);
+        pSprite->setPosition(glm::vec2(300, 100));
 
         GLuint points_vbo = 0;
         glGenBuffers(1, &points_vbo);
@@ -140,7 +151,12 @@ int main(int argc, char** argv)
         // Создаем фруструм для ортографической матрицы
         glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(g_windowSize.x), 0.f, static_cast<float>(g_windowSize.y), -100.f, 100.f);
 
-        pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix); // Задаем проекционную матрицу
+        pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix); // Задаем проекционную матрицу 
+
+
+        pSpriteShaderProgram->use();
+        pSpriteShaderProgram->setInt("tex", 0);
+        pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(pWindow))
@@ -158,6 +174,8 @@ int main(int argc, char** argv)
 
             pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
             glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            pSprite->render();
 
             /* Swap front and back buffers */
             glfwSwapBuffers(pWindow);
